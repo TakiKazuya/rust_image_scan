9837431d12bf403e1ff4c6ec236f6397d363dc48
滝和弥 <kazup0689@gmail.com>
2022-11-07 22:50:50 +0900

    文字列をやりとりする場合はCの文字列として渡ってくるため、c_charを使用するようにした

--- main.rb
+++ main.rb
@@ -3,7 +3,13 @@ require 'ffi'
 module OCR
   extend FFI::Library
   ffi_lib "target/release/librust_image_scan.dylib"
-  attach_function :run, [], :string
+  attach_function :run,
+                  [
+                    :string, # 読込元のパス
+                    :string, # 出力先のパス
+                  ],
+                  :string # 出力先のパス
 end
 
-p OCR.run
+p OCR.run("image.jpg", "output.jpg")
+# "output.jpg"
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
+extern crate core;
+
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
+
 #[no_mangle]
-pub extern fn run(path: String) -> String {
-    println!("path: {}", path);
-    "hoge".to_string()
+pub extern fn run(src_path: *const c_char, dst_path: *const c_char) -> *const c_char {
+    let src_path = unsafe { CStr::from_ptr(src_path).to_str().unwrap() };
+    let dst_path = unsafe { CStr::from_ptr(dst_path).to_str().unwrap() };
+
+    println!("src_path: {:?}", &src_path);
+    println!("dst_path: {:?}", &dst_path);
+
+    CString::new(dst_path).unwrap().into_raw()
 }
